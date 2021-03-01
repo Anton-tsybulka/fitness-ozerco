d6baf9d046044ad8521b2ed82df6e392631122a9
Anton Tsybulka <anton.kurto@gmail.com>
2021-03-01 11:37:25 +0300

    create app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+      </BrowserRouter>
+    </Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
